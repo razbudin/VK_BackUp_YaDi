22f1caed4efed656db69ec785d4b87a218a996fe
razbudin <razbudin@yandex.ru>
2024-05-19 14:55:44 +0300

    Добавлен класс WorkJson создает файл path.json с параметрами для загрузки фото

--- main.py
+++ main.py
@@ -3,56 +3,81 @@ import sys
 import requests
 import json
 # from pprint import pprint
-import vk_token
-import ya_poligon_token
-vk_token = vk_token.access()
-ya_token = ya_poligon_token.access()
+import access
+vk_token = access.vk_key()
+ya_token = access.ya_key()
+''' Для запуска программы создать файл access.py
+c функциями vk_key возвращающая ВК-токен
+и функцию ya_key  возвращающей Я-токен
+Либо просто назначить переменным в этом файле данные значения
+Отключив import access'''
 
-# Словарь параметров по умолчанию для ВК
-input_param = {'count': 5, 'album_id': 'profile', 'rev': 1}
-while 1:
+flag = True
+while flag:
     ''' распределяем параметры по словарю
     если ошибки, выдаем повторный запрос ввода '''
+    # Словарь параметров по умолчанию для ВК
+    input_param = {'count': 5, 'album_id': 'profile', 'rev': 1}
 
-    print('Введите user_id вконтакте (обязательный параметр)')
+    print('Введите user_id вконтакте (обязательный параметр) '
+          'В виде алиаса string, или число int')
+    id = input()
     print('Дополнительно можно ввести, через пробел, в одну строку')
     print('Первый указаный в примере параметр == параметр по умолчанию')
-    print(f'id(только цифры, либо строковый алиас) '
-          f'count=(5, колличество копируемых фото) '
-          f'album_id=(profile или wall) '
-          f'(другие параметры album_id пока не поддерживаются)'
+    print(f'count=(5, колличество копируемых фото) '
+          f'album_id=(profile или wall '
+          f'другие параметры album_id пока не поддерживаются) '
           f'rev=(1 или 0, 0-хронологический, 1-обратный)')
-    print('Вводить только значения либо число, либо строка')
+    print('Вводить только значения число, либо строка. '
+          'Или просто нажмите Enter')
     id_and_param = list(input().split())
-    id = id_and_param.pop(0)
-    print(id_and_param)
+    # id = id_and_param.pop(0)
     if len(id_and_param) < 1 or len(id_and_param) > 3:
         break
     for i in range(len(id_and_param)):
         if (i == 0) and id_and_param[i].isdigit():
             input_param['count'] = id_and_param[i]
-        elif (i == 1) and id_and_param[i].isalpha():
+            flag = False
+        elif (i == 1) and id_and_param[i].lower() in ['profile', 'wall']:
             input_param['album_id'] = id_and_param[i]
+            flag = False
         elif (i == 2) and id_and_param[i] in '01':
             input_param['rev'] = id_and_param[i]
+            flag = False
         else:
             if i == 0:
                 print('Неверное значение count')
+                flag = True
             elif i == 1:
                 print('Неверное значение album_id')
+                flag = True
             elif i == 2:
                 print('Неверное значение rev')
+                flag = True
 
 
 # print('Введите Яндекс Полигон OAuth-токен')
 # ya_token = input()
 # print(access_token)
-print(input_param)
+# print(input_param)
+class Reboot:
+    '''Перезагрузка скрипта'''
+
+    def reboot(self):
+        print('Хотите перезапустить программу y/n')
+        if input() == 'y':
+            print('Restarting...')
+            # Данная перезагрузка подойдет для Linux и Mac
+            os.execv(sys.executable, ['python3'] + sys.argv)
+        else:
+            exit()
+
+
+re = Reboot()
 
 
 class VK:
     ''' Класс для работы с сайтом vk.ru'''
-    photo_size = 'wzyrqpoxms'
 
     def __init__(self, vk_token, id,
                  count=input_param['count'],
@@ -90,17 +115,19 @@ class VK:
             response = requests.get(
                 url, params={**self.params, **params})
             id = response.json()['response'][0]['id']
+            if response.json()['response'][0]['is_closed']:
+                print('Профиль приватный. Невозможно получить фото')
+                re.reboot()
             return id
         except IndexError:
             print('Похоже данный id не существует')
-            print('Restarting...')
-            # Данная перезагрузка подойдет для Linux и Mac
-            os.execv(sys.executable, ['python3'] + sys.argv)
+            re.reboot()
         except KeyError:
             print('Такая вот вышла ошибочка')
             print(response.json()['error']['error_msg'])
+            re.reboot()
 
-    def photos_get(self, count=5, album_id='profile', rev=1):
+    def photos_get(self, count, album_id, rev):
         ''' Получаем .json для дальнейшей работы с фотографиями '''
         # print(self._id, 'user_id photos_get')
         params = {'owner_id': self._id,
@@ -115,10 +142,59 @@ class VK:
         return response.json()
 
     def photos_json(self, *args, **kwargs):
+        '''*args, **kwargs для возможности передачи параметров 
+        напрямую в photos_get.
+        В данной реализации такая возможность не используется '''
         with open('photos.json', 'wt', encoding='utf-8') as outf:
-            json.dump(self.photos_get(), outf)
+            json.dump(self.photos_get(self.count,
+                                      self.album_id,
+                                      self.rev), outf)
+        print('photos.json файл получен')
 
     id = property(get_id, set_id)
+
+
+# Создаем обект класса ВК и запускаем формирование .json файла
+vk = VK(vk_token, id)
+vk.photos_json()
+
+
+class WorkJson:
+
+    def open_json(self):
+        with open('photos.json', 'r', encoding='utf-8') as f:
+            json_data = json.load(f)
+
+        if json_data.get('error') is not None:
+            print('Файл.json не содержит данных для обработки фото')
+            print('Программа будет прекращена')
+            re.reboot()
+        return json_data
+
+    # @staticmethod
+    def create_path(self):
+        json_data = self.open_json()
+        data = []
+        if int(input_param['count']) > int(json_data['response']['count']):
+            count = json_data['response']['count']
+        else:
+            count = int(input_param['count'])
+        for i in range(count):
+            size = json_data['response']['items'][i]['sizes'][-1]['type']
+            likes = json_data['response']['items'][i]['likes']['count']
+            url = json_data['response']['items'][i]['sizes'][-1]['url']
+            date = json_data['response']['items'][i]['date']
+            data.append({'size': size,
+                         'likes': likes,
+                         'url': url,
+                         'date': date})
+        with open('path.json', 'wt', encoding='utf-8') as f:
+            json.dump(data, f)
+        print('path.json сформирован')
+
+
+wj = WorkJson()
+wj.create_path()
 
 
 class YaDi:
@@ -127,15 +203,9 @@ class YaDi:
     def __init__(self, ya_token):
         self.token = ya_token
 
+    def create_folder(self, name='VK_BackUp'):
+        pass
 
-class WorkJson:
-    pass
-
-
-vk = VK(vk_token, id)
-# print(vk.user_info())
-# print(vk.id)
-vk.photos_json()
 # pprint(vk.photos_get())
 # # print(vk.user_get_id())
 # os.remove('photos.json')
